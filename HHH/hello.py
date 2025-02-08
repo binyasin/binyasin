62619d47701202822cb37570d1b4a9c3ad86a400
binyasin <binyasin39@gmail.com>
2025-02-08 21:19:53 +0500

    first

---
+++ HHH/hello.py
@@ -0,0 +1,12 @@
+def main():
+    print("Hello from hhh!")
+
+
+if __name__ == "__main__":
+    main()
+[project]
+name = "hello-world"
+version = "0.1.0"
+description = "Add your description here"
+readme = "README.md"
+dependencies = []
